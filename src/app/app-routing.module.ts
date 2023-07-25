8985fdd6d585b1cd79d4b58ee887c254017ac02e
danielalexis <danielalexis.o.pereira@gmail.com>
2023-07-25 19:16:19 +0100

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitialComponent } from "./initial/initial.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: InitialComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
